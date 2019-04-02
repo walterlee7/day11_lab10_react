8c4a99c0740b98c4f355b1f4377021a116112563
walterlee <walter.lee9@gmail.com>
2019-04-01 19:53:19 -0500

    update

--- chirper/src/index.js
+++ chirper/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import Header from './components/header';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Header />
-    </BrowserRouter>, document.getElementById('root'));
+    </HashRouter>, document.getElementById('root'));
 
 registerServiceWorker();
