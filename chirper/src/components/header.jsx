df8547fe52423d11b964edcd523723ea1e1aa31a
walterlee <walter.lee9@gmail.com>
2019-04-01 20:07:07 -0500

    cleanup

--- chirper/src/components/header.jsx
+++ chirper/src/components/header.jsx
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import List from './list';
 
-
 class Header extends Component {
     render() {
-        console.log("This is the process.env", process.env.PUBLIC_URL);
         return (
             <div className="App">
                 <header className="App-header">
--- chirper/src/index.js
+++ chirper/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import Header from './components/header';
 
-ReactDOM.render(
-    <HashRouter>
-        <Header />
-    </HashRouter>, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('root'));
 
 registerServiceWorker();
