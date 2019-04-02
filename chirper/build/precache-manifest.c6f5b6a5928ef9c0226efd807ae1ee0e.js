e73879f0726be3b9715aa75887f6ade59c84e807
walterlee <walter.lee9@gmail.com>
2019-04-01 20:08:58 -0500

    update

---
+++ chirper/build/precache-manifest.c6f5b6a5928ef9c0226efd807ae1ee0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/walterlee7/day11_lab10_react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "847877c64318f411f8a6",
+    "url": "/walterlee7/day11_lab10_react/static/js/runtime~main.2977a0d7.js"
+  },
+  {
+    "revision": "67cbbb4f6d0b09da808e",
+    "url": "/walterlee7/day11_lab10_react/static/js/main.e269313c.chunk.js"
+  },
+  {
+    "revision": "8d2d4f8ac0031a064d29",
+    "url": "/walterlee7/day11_lab10_react/static/js/2.c45f6693.chunk.js"
+  },
+  {
+    "revision": "7b28022e3d5b34b2307141376d8114a7",
+    "url": "/walterlee7/day11_lab10_react/index.html"
+  }
+];
