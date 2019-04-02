3413171b84a273666c5444f8fa9bc818cb3f8aec
walterlee <walter.lee9@gmail.com>
2019-04-01 19:29:43 -0500

    Updates

--- chirper/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import Header from './components/header';
-
-ReactDOM.render(
-    <Header />, document.getElementById('root'));
-
-registerServiceWorker();
